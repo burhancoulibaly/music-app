d1c5cdf4274b3cad8071adb96d6a54d1d2ec0b72
burha <burhancoulibaly>
2017-02-28 18:16:27 -0500

    added backend

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+var logger = require('morgan'),
+cors = require('cors'),
+http = require('http'),
+express = require('express'),
+errorhandler = require('errorhandler'),
+dotenv = require('dotenv'),
+bodyParser = require('body-parser'),
+request = require('request');
+
+var app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(function(err, req, res, next) {
+  if (err.name === 'StatusError') {
+    res.send(err.status, err.message);
+  } else {
+    next(err);
+  }
+  res.setHeader('Access-Control-Allow-Origin','*');
+});
+request.method === 'OPTIONS'
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(logger('dev'));
+  app.use(errorhandler());
+};
+
+var port = process.env.PORT || 3002;
+
+app.get("/", (req,res)=>{
+	var greeting = "Hello There";
+	res.send(greeting);
+})
+
+http.createServer(app).listen(port, function(err){
+	console.log('listening in http://localhost: ' + port)
+})
+
