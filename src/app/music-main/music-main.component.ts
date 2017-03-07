d3ffc1eeebbeedf2f4984dc9770e16b8e41bba15
burhancoulibaly <burhancoulibaly@gmail.com>
2017-03-07 17:57:31 -0500

    Revert "redid carousel"
    This reverts commit f4cf929242274bc85dc32bac10803dd046be5b7f.

--- src/app/music-main/music-main.component.ts
+++ src/app/music-main/music-main.component.ts
@@ -14,10 +14,12 @@ export class MusicMainComponent implements OnInit {
   artists: Artist[];
   artistslist: Artist[] = [];
   w: number;
+  h: number;
   picnum: number;
   prevPicNum: number = 0;
   count: number = 0;
-  index:number = 0;
+  i: number = 0;
+  startI:number = 0;
   isGreyedLeft:boolean;
   isGreyedRight:boolean;
 
@@ -44,54 +46,80 @@ export class MusicMainComponent implements OnInit {
   }
 
   getArtistCallback(){
+    if(this.count - this.prevPicNum <= 0){
+      this.isGreyedRight = false;
+      this.isGreyedLeft = true;
+    }else if(this.count + this.prevPicNum >= this.artists.length-1){
+      this.isGreyedLeft = false;
+      this.isGreyedRight = true;
+    }else{
+      this.isGreyedRight = false;
+      this.isGreyedLeft = false;
+    }
+    console.log("gACprev1: "+this.prevPicNum)
+    console.log("gACi1: "+this.i);
     this.artistslist = [];
     this.w = window.innerWidth;
     this.picnum = Math.floor(this.w/160)-1;
-    for(var i = 0 - this.prevPicNum; i < i + this.picnum; i++){
-      if(this.artists[i].thumb != null){
-        this.artistslist.push(this.artists[i]);
-      }else{ 
-        continue;
+
+    for(this.i = this.i - this.prevPicNum; this.i < this.count + (this.picnum); this.i++){
+      if(this.artists[this.i].thumb != null){
+        this.artistslist.push(this.artists[this.i]);
       }
     }
-    this.index = i;
+
     this.prevPicNum = this.picnum;
-    console.log("picnum: "+this.picnum);
-    console.log("prevPicNum: "+this.prevPicNum);
     console.log(this.artistslist);
   }
 
   updateCarousel(){
-    console.log("uCi1: "+i);
-    console.log("prevPicNum: "+this.prevPicNum);
-    console.log("uCprev1: "+(this.prevPicNum+this.prevPicNum));
+    if(this.count + this.prevPicNum >= this.artists.length-1){
+      this.isGreyedRight = true;
+      this.isGreyedLeft = false;
+    }else if(this.count - this.prevPicNum <= 0){
+      this.isGreyedLeft = true;
+      this.isGreyedRight = false;
+    }
+    else{
+      this.isGreyedLeft = false;
+      this.isGreyedRight = false;
+    }
     this.artistslist = [];
-    for(var i  = this.index; i <  this.index + this.picnum; i++){
-      if(this.artists[i].thumb != null){
-        this.artistslist.push(this.artists[i]);
-      }else{ 
-        continue;
+    console.log("uCi1: "+this.i);
+    console.log("uCprev1: "+this.prevPicNum);
+    this.count = this.count + this.prevPicNum;
+    console.log("count: "+this.count);
+    for(this.i = this.count; this.i <  this.count + this.prevPicNum; this.i++){
+      if(this.artists[this.i].thumb != null){
+        this.artistslist.push(this.artists[this.i]);
       }
     }
-    this.index = i;
-    console.log("prevPicNum: "+this.prevPicNum)
     console.log(this.artistslist);
   }
 
   updateCarouselBack(){
-    console.log("prevPicNum: "+this.prevPicNum);
+    if(this.count + this.prevPicNum >= this.artists.length-1){
+      this.isGreyedRight = true;
+      this.isGreyedLeft = false;
+    }else if(this.count - this.prevPicNum<= 0){
+      this.isGreyedLeft = true;
+      this.isGreyedRight = false;
+    }
+    else{
+      this.isGreyedLeft = false;
+      this.isGreyedRight = false;
+    }
+    this.artistslist = [];
     console.log("uCBi1: "+(this.count - this.prevPicNum));
-    console.log("uCBprev1: "+(this.prevPicNum+this.prevPicNum)); 
-    this.artistslist = [];   
-    for(var i  = this.index - (this.prevPicNum * 2); i < this.index - this.prevPicNum; i++){
-      if(this.artists[i].thumb != null){
-        this.artistslist.push(this.artists[i]);
-      }else{ 
+    console.log("uCBprev1: "+this.prevPicNum);    
+    for(this.i = this.count - this.prevPicNum; this.i < this.count; this.i++){
+      if(this.artists[this.i].thumb != null){
+        this.artistslist.push(this.artists[this.i]);
+      }else{
         continue;
       }
     }
-    this.index = i - this.prevPicNum;
-    console.log("prevPicNum: "+this.prevPicNum)
+    this.count = this.count - this.prevPicNum;
     console.log(this.artistslist);
   }
   
